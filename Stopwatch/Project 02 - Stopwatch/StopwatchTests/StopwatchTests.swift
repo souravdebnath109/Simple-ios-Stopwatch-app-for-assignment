30d615162752a500e161ca672fc308a3762fb358
souravdebnath109 <souravdebnath54474@gmail.com>
2024-11-13 17:42:35 +0600

    first commit

---
+++ Stopwatch/Project 02 - Stopwatch/StopwatchTests/StopwatchTests.swift
@@ -0,0 +1,27 @@
+
+
+import XCTest
+
+class StopwatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+       
+        super.tearDown()
+    }
+    
+    func testExample() {
+       
+    }
+    
+    func testPerformanceExample() {
+      
+        self.measure {
+                    }
+    }
+    
+}
